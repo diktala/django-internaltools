fb0aa4530cdaf01592fe997e482a08990d6d64df
scubasound <scubasound@panda.local>
2023-01-16 18:15:38 -0500

    add experimental database db2.sqlite

--- internaltools/internaltools/settings.py
+++ internaltools/internaltools/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    'db2': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db2.sqlite3',
+    }
 }
 
 
