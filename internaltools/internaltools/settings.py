4f0a2bfda4b979a78e75e76aa468275e310b539e
scubasound <scubasound@panda.local>
2023-01-16 18:50:37 -0500

    fix settings database db2.sqlite

--- internaltools/internaltools/settings.py
+++ internaltools/internaltools/settings.py
@@ -77,11 +77,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
     'db2': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db2.sqlite3',
-    }
+    },
 }
 
 
