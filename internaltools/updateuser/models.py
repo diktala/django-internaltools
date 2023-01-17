a069005d8cc0c712375c8bdff88b1993f1d6c418
scubasound <scubasound@panda.local>
2023-01-16 19:11:27 -0500

    add experimental model UsersId

--- internaltools/updateuser/models.py
+++ internaltools/updateuser/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class UsersId(models.Model):
+    LoginName = models.CharField(
+        primary_key=True,
+        max_length=20
+    )
+    FirstName = models.CharField(max_length=30)
+    LastName = models.CharField(max_length=30)
+    class Meta:
+       managed = False
+       db_table = 'UsersId'
