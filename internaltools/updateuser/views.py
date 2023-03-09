b1bf935899e525d0495b9462b790e165b627404b
scubasound <scubasound@panda.local>
2023-03-09 17:57:11 -0500

    add django form

--- internaltools/updateuser/views.py
+++ internaltools/updateuser/views.py
@@ -1,11 +1,30 @@
 import os
 from django.shortcuts import render
 from django.http import HttpResponse
-# from .models import UsersId
+from django import forms
 from modelmssql import queryDBall, queryDBrow, queryDBscalar
 
+class SearchLoginForm(forms.Form):
+    loginName = forms.CharField(
+        label='Login Name',
+        max_length=16,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'enter login name ...',
+            'class': 'form-control',
+        })
+    )
+
 def index(request):
+    if request.method == 'POST':
+        form = SearchLoginForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('thanks')
+    else:
+        defaultData = {'loginName': 'some default id',}
+        form = SearchLoginForm(defaultData)
+
     # example query using django models
+    # add this: from .models import UsersId
     # myResult = UsersId.objects.using('db2')
     # myResultList = (myResult.values()[0]['LoginName'])
 
@@ -20,6 +39,7 @@ def index(request):
                'myTax': myMssqlResultSingle,
                'domain': DOMAIN,
                'urlQuery': urlQuery,
+               'form': form,
     }
     # return HttpResponse(f"Hello, the Tax1 is {myMssqlResultSingle}")
     return render(request, 'updateuser/sample.html', context)
