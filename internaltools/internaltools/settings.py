01fd5c20dc114ec97a569c7056de27072b06b9ee
sardaukar <sardaukar@sardaukar>
2023-09-14 16:35:44 +0000

    fix static files on sardaukar

--- internaltools/internaltools/settings.py
+++ internaltools/internaltools/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.urandom(12)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sardaukar.vif.com']
 
 
 # Application definition
