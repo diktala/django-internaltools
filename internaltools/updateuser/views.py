12a068ab55b91d6df403baa4aee21360eaf3fa1a
sardaukar <sardaukar@sardaukar>
2023-09-15 00:45:54 +0000

    updateuser add fields

--- internaltools/updateuser/views.py
+++ internaltools/updateuser/views.py
@@ -6,80 +6,464 @@ from django.core.validators import RegexValidator
 import re
 from modelmssql import queryDBall, queryDBrow, queryDBscalar
 
+
 class FormSearchLogin(forms.Form):
     loginName = forms.CharField(
-        label='Login Name',
-        widget=forms.TextInput(attrs={
-            'placeholder': 'Customer username ...',
-            'class': 'form-control',
-        }),
+        label="Login Name",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Customer username ...",
+                "class": "form-control",
+            }
+        ),
         required=True,
         min_length=1,
         max_length=20,
         validators=[
             RegexValidator(
-                regex='^[a-z0-9][a-z0-9.-]*$',
-                message='invalid characters',
+                regex="^[a-z0-9][a-z0-9.-]*$",
+                message="invalid characters",
                 flags=re.IGNORECASE,
             )
         ],
     )
 
+
 class FormUserDetail(forms.Form):
     loginName = forms.CharField(
+        widget=forms.HiddenInput(),
         disabled=True,
     )
     firstName = forms.CharField(
-        label='First Name',
-        widget=forms.TextInput(attrs={
-            'placeholder': ' ...',
-            'class': 'form-control',
-        }),
+        label="First Name",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
         required=True,
         min_length=1,
         max_length=30,
         validators=[
             RegexValidator(
                 regex="^[\w. &'-]*[\w]$",
-                message='invalid characters',
+                message="invalid characters",
             )
         ],
     )
     lastName = forms.CharField(
-        label='Last Name',
-        widget=forms.TextInput(attrs={
-            'placeholder': ' ...',
-            'class': 'form-control',
-        }),
+        label="Last Name",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
         required=True,
         min_length=1,
         max_length=30,
         validators=[
             RegexValidator(
                 regex="^[\w. &'-]*[\w]$",
-                message='invalid characters',
+                message="invalid characters",
+            )
+        ],
+    )
+    organizationName = forms.CharField(
+        label="Organization Name",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[\w. &'-]*[\w.]$",
+                message="invalid characters",
+            )
+        ],
+    )
+    address = forms.CharField(
+        label="Address",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[\w. &'-]*[\w.]$",
+                message="invalid characters",
+            )
+        ],
+    )
+    city = forms.CharField(
+        label="City",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[\w. &'-]*[\w.]$",
+                message="invalid characters",
+            )
+        ],
+    )
+    state = forms.CharField(
+        label="State",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[\w. &'-]*[\w.]$",
+                message="invalid characters",
+            )
+        ],
+    )
+    postalCode = forms.CharField(
+        label="Postal Code",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[A-Z][0-9][A-Z] [0-9][A-Z][0-9]$",
+                message="must use a canadian postal code H1H 1H1",
+            )
+        ],
+    )
+    homePhone = forms.CharField(
+        label="Home Phone",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[0-9]{3} [0-9]{3} [0-9]{4}$",
+                message="must use format 514 555 1212",
+            )
+        ],
+    )
+    accountNumber = forms.CharField(
+        label="Account Number",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[0-9]{1,9}$",
+                message="must be 9-digit ex. 800-555-1234 becomes 855512341",
+            )
+        ],
+    )
+    language = forms.CharField(
+        label="Language",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^EN$",
+                message="Choose EN or empty",
+            )
+        ],
+    )
+    paymentMethod = forms.CharField(
+        label="Payment Method",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^VISA|MC$",
+                message="Choose VISA or MC or empty",
+            )
+        ],
+    )
+    creditCardNumber = forms.CharField(
+        label="Credit Card Number",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[0-9 ]{16,19}$",
+                message="must be empty or a credit card number 4111111111111111",
+            )
+        ],
+    )
+    creditCardExpiry = forms.CharField(
+        label="Credit Card Expiry",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[0-9]{4}-[0-9]{2}-[0-9]{2}$",
+                message="YYYY-MM-DD ex:2030-11-30",
+            )
+        ],
+    )
+    bankName = forms.CharField(
+        label="Bank Name",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[\w. &'-]*[\w.]$",
+                message="invalid characters",
+            )
+        ],
+    )
+    checkNumber = forms.CharField(
+        label="Check Number",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[\w. &'-]*[\w.]$",
+                message="invalid characters",
+            )
+        ],
+    )
+    bankAccount = forms.CharField(
+        label="Bank Account",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[\w. &'-]*[\w.]$",
+                message="invalid characters",
+            )
+        ],
+    )
+    identificationCard = forms.CharField(
+        label="identificationCard",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[\w. &'-]*[\w.]$",
+                message="invalid characters",
+            )
+        ],
+    )
+    authorizationCode = forms.CharField(
+        label="Authorization Code",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[\w. &'-]*[\w.]$",
+                message="invalid characters",
+            )
+        ],
+    )
+    operatingSystem = forms.CharField(
+        label="Operating System",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[\w. &'-]*[\w.]$",
+                message="invalid characters",
+            )
+        ],
+    )
+    operator = forms.CharField(
+        label="operator",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[\w. &'-]*[\w.]$",
+                message="invalid characters",
+            )
+        ],
+    )
+    referredBy = forms.CharField(
+        label="Referred By",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[\w. &'-]*[\w.]$",
+                message="invalid characters",
+            )
+        ],
+    )
+    notes = forms.CharField(
+        label="Notes",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": " ...",
+                "class": "form-control",
+            }
+        ),
+        required=True,
+        min_length=1,
+        max_length=250,
+        validators=[
+            RegexValidator(
+                regex="^[\w. &'-]*[\w.]$",
+                message="invalid characters",
+            )
+        ],
+    )
+    dateJoined = forms.CharField(
+        label="Date Joined",
+        widget=forms.HiddenInput(),
+        disabled=True,
+        min_length=1,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex="^[0-9]{4}-[0-9]{2}-[0-9]{2}( 00:00:00)?$",
+                message="incorrect characters used 2000-01-01",
             )
         ],
     )
 
 
 def index(request):
-    defaultData = {'loginName': '',}
+    defaultData = {
+        "loginName": "",
+    }
     formSearchLogin = FormSearchLogin(defaultData)
     formUserDetail = FormUserDetail(defaultData)
 
     if request.GET.get("loginName"):
-        print (F"DEBUG MESSAGE: method GET {request.GET.get('loginName')} ")
+        print(f"DEBUG MESSAGE: method GET {request.GET.get('loginName')} ")
         formSearchLogin = FormSearchLogin(request.GET)
 
-    if request.method == 'POST':
-        print ('DEBUG MESSAGE: method POST')
+    if request.method == "POST":
+        print("DEBUG MESSAGE: method POST")
         formUserDetail = FormUserDetail(request.POST)
         if formSearchLogin.is_valid():
-            print ('DEBUG MESSAGE: form is valid')
+            print("DEBUG MESSAGE: form is valid")
             # return HttpResponse('thanks')
         else:
-            print ('DEBUG MESSAGE: form is NOT valid')
+            print("DEBUG MESSAGE: form is NOT valid")
             # return HttpResponse('form is invalid')
 
     # example query using django models
@@ -88,18 +472,19 @@ def index(request):
     # myResultList = (myResult.values()[0]['LoginName'])
 
     # example query using modelmssql
-    myMssqlResult = queryDBall('SELECT * FROM Taxes')
-    myMssqlResultSingle = myMssqlResult[0]['Tax1']
+    myMssqlResult = queryDBall("SELECT * FROM Taxes")
+    myMssqlResultSingle = myMssqlResult[0]["Tax1"]
 
     DOMAIN = os.environ.get("DOMAIN")
-    loginName = 'test123'
+    loginName = "test123"
     urlQuery = f"LoginName={loginName}"
-    context = {'test': '123 test',
-               'myTax': myMssqlResultSingle,
-               'domain': DOMAIN,
-               'urlQuery': urlQuery,
-               'formSearchLogin': formSearchLogin,
-               'formUserDetail': formUserDetail,
+    context = {
+        "test": "123 test",
+        "myTax": myMssqlResultSingle,
+        "domain": DOMAIN,
+        "urlQuery": urlQuery,
+        "formSearchLogin": formSearchLogin,
+        "formUserDetail": formUserDetail,
     }
     # return HttpResponse(f"Hello, the Tax1 is {myMssqlResultSingle}")
-    return render(request, 'updateuser/sample.html', context)
+    return render(request, "updateuser/sample.html", context)
