011c916c8959d788d143a1428045eed09861c3cd
sardaukar at host <sardaukar@host>
2023-10-01 21:04:25 -0400

    commons

--- internaltools/commons.py
+++ internaltools/commons.py
@@ -1,5 +1,16 @@
 from datetime import datetime, timedelta
 import re
+from modelmssql import queryDBscalar
+
+
+def getDateFormatted( initialDate=''):
+    formattedDate = ""
+    if isinstance(initialDate, datetime):
+        formattedDate = initialDate.strftime("%Y-%m-%d")
+    if isinstance(initialDate, str):
+        formattedDate = re.sub('([0-9]{4}).([0-9]{2}).([0-9]{2}).*',r'\1-\2-\3', initialDate)
+    if not isDateValid(formattedDate): formattedDate=""
+    return formattedDate
 
 
 def isDateValid(dateToCheck):
@@ -34,7 +45,7 @@ def sanitizeLogin(loginName):
     return loginSanitized
 
 
-def getInvoiceNumberFromInvoiceString(invoiceString=""):
+def get_invoicenumber_from_obfuscated_number(invoiceString=""):
     invoiceNumber = "0"
     #
     if str(invoiceString).isdecimal() and len(str(invoiceString)) < 10:
@@ -45,3 +56,42 @@ def getInvoiceNumberFromInvoiceString(invoiceString=""):
         invoiceNumber = matchResult.group(1)
     #
     return invoiceNumber
+
+
+def count_loginnames_in_database(loginToCheck):
+    loginToCheckSanitized = sanitizeLogin(loginToCheck)
+    loginFound = queryDBscalar(
+        f"SELECT count(*) FROM UsersId where LoginName = lower('{loginToCheckSanitized}')"
+    )
+    return loginFound
+
+
+def get_loginname_from_invoice(invoiceNumber):
+    invoiceNumberSanitized = sanitizeLogin(invoiceNumber)
+    invoiceNumberDigits = get_invoicenumber_from_obfuscated_number(invoiceNumberSanitized)
+    loginName = queryDBscalar(
+        f"SELECT LoginName FROM Invoices where InvoiceNumber = %s",
+        str(invoiceNumberDigits),
+    )
+    if isinstance(loginName, str):
+        return loginName.strip()
+    else:
+        return ""
+
+
+def get_loginname_from_database(either_login_or_invoice=""):
+    confirmedLoginName = ""
+    if not isinstance(either_login_or_invoice, str):
+        either_login_or_invoice = ""
+    # case loginname is an invoicenumber
+    invoiceNumber = either_login_or_invoice
+    loginNameFromInvoice = get_loginname_from_invoice(invoiceNumber)
+    loginFound = count_loginnames_in_database(loginNameFromInvoice)
+    if str(loginFound) == "1":
+        confirmedLoginName = loginNameFromInvoice
+    # case loginname is normal alias
+    loginName = either_login_or_invoice
+    loginFound = count_loginnames_in_database(loginName)
+    if str(loginFound) == "1":
+        confirmedLoginName = loginName
+    return confirmedLoginName
--- internaltools/internaltools/tests.py
+++ internaltools/internaltools/tests.py
@@ -1,45 +1,77 @@
 from django.test import TestCase
 from django.urls import reverse
-from commons import isDateValid, isDateExpired, getLastDay, sanitizeLogin, getInvoiceNumberFromInvoiceString
+import commons
 
 
 class SimpleTest(TestCase):
 
+    def test_getDateFormatted(self):
+        self.assertEqual( commons.getDateFormatted("2023-01-01"), "2023-01-01" )
+        self.assertEqual( commons.getDateFormatted("2023-01-01blabla"), "2023-01-01" )
+        self.assertEqual( commons.getDateFormatted("2023.01.01blabla"), "2023-01-01" )
+        self.assertEqual( commons.getDateFormatted("2023.91.01"), "" )
+        self.assertEqual( commons.getDateFormatted("some bad data"), "" )
+
+
     def test_isDateValid(self):
-        self.assertTrue( isDateValid("2023-01-01") )
-        self.assertFalse( isDateValid("some bad data") )
+        self.assertTrue( commons.isDateValid("2023-01-01") )
+        self.assertFalse( commons.isDateValid("some bad data") )
 
 
     def test_isDateExpired(self):
-        self.assertTrue( isDateExpired("1930-01-01") )
-        self.assertFalse( isDateExpired("2222-01-01") )
-        self.assertFalse( isDateExpired("BUGBUG") )
+        self.assertTrue( commons.isDateExpired("1930-01-01") )
+        self.assertFalse( commons.isDateExpired("2222-01-01") )
+        self.assertFalse( commons.isDateExpired("BUGBUG") )
 
 
     def test_getLastDay(self):
-        self.assertEqual( getLastDay('2023-01-01'), '2023-01-31' )
-        self.assertEqual( getLastDay('2023-01-31'), '2023-01-31' )
-        self.assertEqual( getLastDay('BUGBUG'), '' )
+        self.assertEqual( commons.getLastDay('2023-01-01'), '2023-01-31' )
+        self.assertEqual( commons.getLastDay('2023-01-31'), '2023-01-31' )
+        self.assertEqual( commons.getLastDay('BUGBUG'), '' )
 
 
     def test_sanitizeLogin(self):
         # re.match(r"^[\w.-]{1,30}$"
-        self.assertEqual( sanitizeLogin('CAPS-Lower123.'), 'CAPS-Lower123.')
-        self.assertEqual( sanitizeLogin('badHAS Space'), '' )
-        self.assertEqual( sanitizeLogin('badHAS:!@#$%^&*'), '' )
-        self.assertEqual( sanitizeLogin('badTooLongSentenceBeyond30chars'), '' )
+        self.assertEqual( commons.sanitizeLogin('CAPS-Lower123.'), 'CAPS-Lower123.')
+        self.assertEqual( commons.sanitizeLogin('badHAS Space'), '' )
+        self.assertEqual( commons.sanitizeLogin('badHAS:!@#$%^&*'), '' )
+        self.assertEqual( commons.sanitizeLogin('badTooLongSentenceBeyond30chars'), '' )
 
 
-    def test_getInvoiceNumberFromInvoiceString(self):
+    def test_get_invoicenumber_from_obfuscated_number(self):
         # re.match("^[0-9]{3}.([0-9]+)[0-9]{3}$"
-        self.assertEqual( getInvoiceNumberFromInvoiceString('999.12345999'), '12345')
-        self.assertEqual( getInvoiceNumberFromInvoiceString('12345'), '12345')
-        self.assertEqual( getInvoiceNumberFromInvoiceString('BUGBUG'), '0')
-        self.assertEqual( getInvoiceNumberFromInvoiceString(''), '0')
-        self.assertEqual( getInvoiceNumberFromInvoiceString('123.345'), '0')
-        self.assertEqual( getInvoiceNumberFromInvoiceString('01234567890123'), '0')
+        self.assertEqual( commons.get_invoicenumber_from_obfuscated_number('999.12345999'), '12345')
+        self.assertEqual( commons.get_invoicenumber_from_obfuscated_number('12345'), '12345')
+        self.assertEqual( commons.get_invoicenumber_from_obfuscated_number('BUGBUG'), '0')
+        self.assertEqual( commons.get_invoicenumber_from_obfuscated_number(''), '0')
+        self.assertEqual( commons.get_invoicenumber_from_obfuscated_number('123.345'), '0')
+        self.assertEqual( commons.get_invoicenumber_from_obfuscated_number('01234567890123'), '0')
 
-"""
+
+    def test_count_loginnames_in_database(self):
+        self.assertEqual( commons.count_loginnames_in_database("trial"), 1)
+        self.assertEqual( commons.count_loginnames_in_database("user_is_inexistant"), 0)
+
+
+    def test_get_loginname_from_invoice(self):
+        self.assertEqual( commons.get_loginname_from_invoice("1000"), "ammina")
+        self.assertEqual( commons.get_loginname_from_invoice("123.1000123"), "ammina")
+
+
+    def test_get_loginname_from_database(self):
+        self.assertEqual( commons.get_loginname_from_database("trial"), "trial")
+        self.assertEqual( commons.get_loginname_from_database("1000"), "ammina")
+        self.assertEqual( commons.get_loginname_from_database("123.1000123"), "ammina")
+        self.assertEqual( commons.get_loginname_from_database("user_is_inexistant"), "")
+        self.assertEqual( commons.get_loginname_from_database("user_#$_inexistant"), "")
+        self.assertEqual( commons.get_loginname_from_database("TRial"), "TRial")
+        self.assertEqual( commons.get_loginname_from_database(""), "")
+        self.assertEqual( commons.get_loginname_from_database(1000), "")
+        self.assertEqual( commons.get_loginname_from_database(None), "")
+        self.assertEqual( commons.get_loginname_from_database("has spaces"), "")
+
+
+    """
     def test_form_validation_valid(self):
         formSearchLogin = FormSearchLogin( { "loginName": "sometest"} )
         self.assertTrue( formSearchLogin.is_valid() )
@@ -51,19 +83,5 @@ class SimpleTest(TestCase):
     def test_form_validation_invalid_bad_characters(self):
         formSearchLogin = FormSearchLogin( { "loginName": "some!@$%test"} )
         self.assertFalse( formSearchLogin.is_valid() )
+    """
 
-    def test_countConfirmedLoginName(self):
-        self.assertEqual( countConfirmedLoginName("trial"), 1)
-        self.assertEqual( countConfirmedLoginName("user_is_inexistant"), 0)
-
-    def test_getLoginNameFromInvoiceNumber(self):
-        self.assertEqual( getLoginNameFromInvoiceNumber("1000") , "ammina")
-        self.assertEqual( getLoginNameFromInvoiceNumber("1001") , "patgar")
-
-    def test_getConfirmedLoginName(self):
-        self.assertEqual( getConfirmedLoginName('1000'), 'ammina')
-        self.assertEqual( getConfirmedLoginName('1001'), 'patgar')
-        self.assertEqual( getConfirmedLoginName('123.1001432'), 'patgar')
-        self.assertEqual( getConfirmedLoginName('patgar'), 'patgar')
-        self.assertEqual( getConfirmedLoginName('something555'), '')
-"""
